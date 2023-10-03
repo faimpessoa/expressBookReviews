d3180e17c25affbf11ac3ed8c7e7773a0a8f2275
Manuel Pessoa <faimpessoa@gmail.com>
2023-10-03 12:49:36 -0400

    Promise for GetByAuthor and GetByISBN implemented
    Signed-off-by: Manuel Pessoa <faimpessoa@gmail.com>

--- router/general.js
+++ router/general.js
@@ -23,17 +23,27 @@ public_users.post("/register", (req,res) => {
 
 
 // Get the book list available in the shop
+/*
 public_users.get('/',function (req, res) {
   //Write your code here
-  const promGetAll = new Promise((resolve, reject) => {
-    resolve([200, JSON.stringify(books)]);
-  });
-
-  promGetAll.then((resp_data) => {return res.send(resp_data[1]).status(resp_data[0])});
- 
+  return res.send(JSON.stringify(books)).status(200);
 });
+*/
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+    //Write your code here
+    const promGetAll = new Promise((resolve, reject) => {
+      resolve([200, JSON.stringify(books)]);
+    });
+  
+    promGetAll.then((resp_data) => {return res.send(resp_data[1]).status(resp_data[0])});
+   
+  });
+  
 
 // Get book details based on ISBN
+/*
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   if (req.params.isbn in books) 
@@ -43,8 +53,37 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
   return res.status(404).json({message: "ISBN not found"});
  });
+ */
+
+  public_users.get('/isbn/:isbn',function (req, res) {
+    //Write your code here
+
+    const promGetISBN = new Promise((resolve, reject) => {
+        if (req.params.isbn in books) 
+        {
+            resolve([200, JSON.stringify(books[req.params.isbn])]);
+        }
+        else
+        {
+            resolve([404, "ISBN not found"]);
+        }
+        
+      });
+    
+      promGetISBN.then((resp_data) => {
+        if (resp_data[0] == 200)
+        {  
+            return res.send(resp_data[1]).status(resp_data[0]);
+        }
+
+        return res.status(resp_data[0]).json({errmessage:resp_data[1]});
+    });
+   });
+  
+
   
 // Get book details based on author
+/*
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   let resBooks = [];
@@ -58,6 +97,47 @@ public_users.get('/author/:author',function (req, res) {
 
   return res.send(JSON.stringify(resBooks)).status(200);
 });
+*/
+
+public_users.get('/author/:author',function (req, res) {
+    //Write your code here
+
+    const promGetByAuthor = new Promise((resolve, reject) => {
+
+        let resBooks = [];
+        for (const bk in books)
+        {
+            if (books[bk].author == req.params.author)
+            {
+                resBooks.push(books[bk]);
+            }
+        }
+
+        if (resBooks.length > 0) 
+        {
+            resolve([200, JSON.stringify(resBooks)]);
+        }
+        else
+        {
+            resolve([404, "No books from author " + req.params.author]);
+        }
+        
+      });
+    
+      promGetByAuthor.then((resp_data) => {
+        if (resp_data[0] == 200)
+        {  
+            return res.send(resp_data[1]).status(resp_data[0]);
+        }
+
+        return res.status(resp_data[0]).json({errmessage:resp_data[1]});
+    });
+
+      
+    return res.send(JSON.stringify(resBooks)).status(200);
+  });
+  
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
